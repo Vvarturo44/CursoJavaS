b71232d8fc290a7f2046a2b1a57e35da9ff90bd1
Vvarturo44 <arturo.f.vv@gmail.com>
2025-01-24 07:54:00 -0600

    Actualizando el juego

--- ProyectoNumeroMagico/2034-logica-programacion-1-Aula1/app.js
+++ ProyectoNumeroMagico/2034-logica-programacion-1-Aula1/app.js
@@ -1,18 +1,32 @@
 //Declaracion de Variables
-let numeroSecreto = Math.floor(Math.random()*10)+1; //Variable que genera un numero aleatorio entre 1 y 10 descartando el 0
+let rangoMaximo = 1;
+let numeroSecreto = 1;
 let numeroUsuario = 0;
 let intentos = 1;
+let maximosIntentos = 0;
+
 //let palabraVeces = 'vez'; Variable para cambiar la palabra "vez" a "veces" cuando el jugador se equivoque
-let maximosIntentos = 3; //Variable para definir el numero maximo de intentos
+
 //console.log(numeroSecreto);
 
 alert(`Bienvenido al juego del numero secreto`);
+rangoMaximo = parseInt(prompt(`Ingresa un numero entre 1 y el numero maximo con el que quieras jugar`));
+numeroSecreto = Math.floor(Math.random() * rangoMaximo) + 1; 
 //Mientras el numero ingresado por el usuario sea diferente al numero secreto, se seguira ejecutando el ciclo
 while(numeroUsuario != numeroSecreto){
     //Pedimos un numero al usuario que ingrese un numero y lo guardamos en la variable "numeroUsuario" convirtiendolo a entero
-    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y 10 por favor:`));
     console.log(typeof(numeroUsuario)); //Agreamos un console.log para ver el valor de "numeroUsuario" despues de la entrada del usuario
-
+    if(rangoMaximo>=20){
+        maximosIntentos = 5;
+    } else if(rangoMaximo>=50){
+        maximosIntentos = 10;
+    } else if(rangoMaximo<20 && rangoMaximo>0){
+        maximosIntentos = 3;
+    } else {
+        alert(`Fin del juego, no seguiste las instrucciones`); 
+        break;
+    }
+    numeroUsuario = parseInt(prompt(`Me indicas un numero entre 1 y ${rangoMaximo} por favor. Cuentas con ${maximosIntentos} intentos`));
     //Realizamos la comparacion
     console.log(`Resultado de la comparacion:`, numeroSecreto == numeroUsuario); //Agregamos un console.log para ver el resultado de la comparacion
     if (numeroSecreto == numeroUsuario){ 
@@ -34,10 +48,11 @@ while(numeroUsuario != numeroSecreto){
         intentos++; //Incrementamos el contador de intentos en la parte de la condicion falsa
         //palabraVeces = 'veces'; //Cambiamos la palabra a plural
         if(intentos>maximosIntentos){
-            alert(`Lo siento, has llegado al numero maximo de ${maximosIntentos} intentos`);
+            alert(`Lo siento, has llegado al numero maximo de ${maximosIntentos} intentos, el numero secreto era: ${numeroSecreto}`);
             break;
         }
         console.log(`Valor del numero secreto:`, numeroSecreto); //Agregamos un console.log para ver el valor de "numeroSecreto" cuando el jugador se equivoca
         //alert(`Lo siento, no adivinaste el numero secreto`);
     } 
 }
+
